2eb8a8aa00cf26bad83ba729f3d94b58fd3c9a11
Graeme Clark <grae0026@stud.kea.dk>
2020-03-21 12:56:34 +0100

    add test.json

--- js/script.js
+++ js/script.js
@@ -1,9 +1,47 @@
 "use strict";
 
-console.log("test");
+window.addEventListener("DOMContentLoaded", start);
 
-const data = [
-	{
-		i: "1.jpg"
+gsap.registerPlugin(MotionPathPlugin);
+gsap.from(".anim", { duration: 3, stagger: 1, y: 100 });
+
+// const data = [
+// 	{
+// 		i: "1.jpg"
+// 	}
+// ];
+
+// start function to listen for events
+function start() {
+	// console.log("start");
+	getInstagram();
+}
+
+// fetch Instagram feed from API
+function getInstagram() {
+	console.log("getInstagram");
+	fetch("test.json")
+		.then(response => {
+			return response.json();
+		})
+		.then(allPosts => {
+			allPosts.data.forEach(post => {
+				showData(post);
+				console.log(post.timestamp);
+			});
+		});
+}
+
+function showData(post) {
+	console.log("showData");
+	const template = document.querySelector("template").content;
+	const copy = template.cloneNode(true);
+	// console.log(post);
+	if (post.media_type === "IMAGE") {
+		copy.querySelector(".data_image").src = post.media_url;
+		// copy.querySelector(".data_image").style.display = "none";
+	} else {
+		console.log("Hmmmmm");
 	}
-];
+	document.getElementById("testy").appendChild(copy);
+}

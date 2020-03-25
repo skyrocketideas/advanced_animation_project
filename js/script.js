0bd607f7a85b4a8c5c1e403d5be00c695705b81e
darinatoneva <48431260+darinatoneva@users.noreply.github.com>
2020-03-25 16:17:11 +0100

    Revert "Feature/animations"

--- js/script.js
+++ js/script.js
@@ -16,24 +16,24 @@ function start() {
 }
 
 async function getData() {
-  const api_url = "img/world_2.svg";
-  let response = await fetch(api_url);
-  let data = await response.text();
-  document.querySelector("#world").innerHTML = data;
-  selectBuildings();
-  // console.log(data);
+	const api_url = "img/world_2.svg";
+	let response = await fetch(api_url);
+	let data = await response.text();
+	document.querySelector("#world").innerHTML = data;
+	selectBuildings();
+	// console.log(data);
 }
 
 // select buildings
 function selectBuildings() {
-  console.log("selectBuildings");
-  document.querySelectorAll("#earth, #houseFront").forEach(element => {
-    element.addEventListener("click", function (event) {
-      let chosenPath = event.target;
-      element.classList.add("active");
-      console.log(chosenPath);
-    });
-  });
+	console.log("selectBuildings");
+	document.querySelectorAll("#earth, #houseFront").forEach(element => {
+		element.addEventListener("click", function(event) {
+			let chosenPath = event.target;
+			element.classList.add("active");
+			console.log(chosenPath);
+		});
+	});
 }
 
 // // get color
@@ -81,12 +81,12 @@ function selectBuildings() {
 // 	startManipulatingTheSvg();
 // }
 
-const api_url = "json/scenes.json";
-const response = await fetch(api_url);
-const data = await response.json();
-showSceneThree(data);
-console.log(data);
-
+  const api_url = "json/scenes.json";
+  const response = await fetch(api_url);
+  const data = await response.json();
+  showSceneThree(data);
+  console.log(data);
+}
 
 // or ...
 // can we do this?
@@ -115,8 +115,7 @@ function showSceneThree(data) {
   //document.querySelector("#sceneThreeText").innerHTML = data.scene_three[2].text;
 }
 
-//MODAL
-/*
+
 const modal = document.querySelector(".modal-background");
 modal.addEventListener("click", () => {
   modal.classList.add("hide");
@@ -128,61 +127,4 @@ function showDetails(data) {
   modal.querySelector(".modal-description").textContent = data.longdescription;
   modal.querySelector(".modal-price").textContent = data.price;
   modal.classList.remove("hide");
-}
-*/
-
-//ANIMATIONS screen 2
-/*Animation lights*/
-gsap.from("#birth", {
-  duration: 10,
-  scale: 0.5,
-  ease: "back"
-});
-
-let space = document.getElementById("space"),
-  spaceWidth = space.scrollWidth,
-  spaceHeight = space.scrollHeight,
-  perspective = 100;
-
-space.style.setProperty(`--perspective`, `${perspective}px`);
-
-function makeStar() {
-  const star = document.createElement('time'),
-    starWidth = gsap.utils.random(1, 2, 1),
-    starHeight = starWidth * gsap.utils.random(20, 40, 1),
-    randomRotation = Math.random() * 360,
-    scaleModifier = Math.random();
-
-  const visibleRangeMaximum = spaceWidth - spaceHeight > 0 ? spaceWidth / 2 : spaceHeight / 2;
-
-  gsap.set(star, {
-    width: starWidth,
-    height: starHeight,
-    transform: `
-      translateY(${starHeight / 2}px)
-      rotate(${randomRotation}deg)
-      rotateX(90deg)
-      translate3d(0,0,0px)
-      scaleX(${scaleModifier})
-    `,
-  });
-
-  gsap.to(star, {
-    duration: "random(5, 20)",
-    transform: `
-      translateY(${starHeight / 2}px)
-      rotate(${randomRotation}deg)
-      rotateX(90deg)
-      translate3d(0,0,${perspective + visibleRangeMaximum}px)
-      scaleX(${scaleModifier})
-    `,
-    repeat: -1,
-    ease: "none",
-  }).progress(Math.random());
-
-  space.appendChild(star);
-}
-
-for (let i = 0; i < 200; i++) {
-  makeStar();
 }
